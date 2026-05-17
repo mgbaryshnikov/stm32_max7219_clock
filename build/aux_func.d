2ee1f38e6fa7aefb40583df89669d2f65582ddcb
m_bar_g <mgbaryshnikov@yandex.ru>
2026-05-17 20:42:25 +0300

    build is removed

--- build/aux_func.d
+++
@@ -1,2 +0,0 @@
-build/aux_func.o: Core/Src/aux_func.c Core/Src/../Inc/aux_func.h
-Core/Src/../Inc/aux_func.h:
--- build/bits_flag.d
+++
@@ -1,2 +0,0 @@
-build/bits_flag.o: Core/Src/bits_flag.c Core/Src/../Inc/bits_flag.h
-Core/Src/../Inc/bits_flag.h:
